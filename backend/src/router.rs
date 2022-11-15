2e87efaa75a8a2c404a5ecde90a57fd620918f65
Dawid Mazur <dawid.m@webchefs.pl>
2022-11-15 11:01:43 +0100

    minor reformat

--- backend/src/router.rs
+++ backend/src/router.rs
@@ -7,106 +7,106 @@ pub fn router(route: Route, data: &StandardInput) -> Result<JsonValue, Box<dyn E
     return match route.endpoint.as_str() {
         "get_zones" => get_zones(),
         "check_point_in_zones" => {
-            if let Some(RoutePayload::Point(value)) = route.payload {
-                return check_point_in_zone(value);
+            return if let Some(RoutePayload::Point(value)) = route.payload {
+                check_point_in_zone(value)
             } else {
-                return Err(Box::new(ErrorOutput {
+                Err(Box::new(ErrorOutput {
                     error: "Validation failed! Point does not meet requirements".into(),
-                }));
+                }))
             }
         }
         "buy_ticket" => {
-            if let Some(RoutePayload::Ticket(value)) = route.payload {
+            return if let Some(RoutePayload::Ticket(value)) = route.payload {
                 if let TicketActions::Buy(value) = value {
-                    return buy_ticket(value, &data);
+                    buy_ticket(value, &data)
                 } else {
-                    return Err(Box::new(ErrorOutput {
+                    Err(Box::new(ErrorOutput {
                         error: "Validation failed! Buy Ticket does not meet requirements".into(),
-                    }));
+                    }))
                 }
             } else {
-                return Err(Box::new(ErrorOutput {
+                Err(Box::new(ErrorOutput {
                     error: "Validation failed! Ticket does not meet requirements".into(),
-                }));
+                }))
             }
         }
         "get_tickets" => {
-            if let Some(RoutePayload::Ticket(value)) = route.payload {
+            return if let Some(RoutePayload::Ticket(value)) = route.payload {
                 if let TicketActions::Get(value) = value {
-                    return get_tickets(value);
+                    get_tickets(value)
                 } else {
-                    return Err(Box::new(ErrorOutput {
+                    Err(Box::new(ErrorOutput {
                         error: "Validation failed! Get Ticket does not meet requirements".into(),
-                    }));
+                    }))
                 }
             } else {
-                return Err(Box::new(ErrorOutput {
+                Err(Box::new(ErrorOutput {
                     error: "Validation failed! Ticket does not meet requirements".into(),
-                }));
+                }))
             }
         }
         "validate_ticket" => {
-            if let Some(RoutePayload::Ticket(value)) = route.payload {
+            return if let Some(RoutePayload::Ticket(value)) = route.payload {
                 if let TicketActions::Validate(value) = value {
-                    return validate_ticket(value);
+                    validate_ticket(value)
                 } else {
-                    return Err(Box::new(ErrorOutput {
+                    Err(Box::new(ErrorOutput {
                         error: "Validation failed! Validate Ticket does not meet requirements".into(),
-                    }));
+                    }))
                 }
             } else {
-                return Err(Box::new(ErrorOutput {
+                Err(Box::new(ErrorOutput {
                     error: "Validation failed! Ticket does not meet requirements".into(),
-                }));
+                }))
             }
         }
         "get_app_balance" => {
-            if let Some(RoutePayload::Balance(value)) = route.payload {
+            return if let Some(RoutePayload::Balance(value)) = route.payload {
                 if let BalanceActions::Get(value) = value {
-                    return get_app_balance(&value);
+                    get_app_balance(&value)
                 } else {
-                    return Err(Box::new(ErrorOutput {
+                    Err(Box::new(ErrorOutput {
                         error: "Validation failed! Get Balance does not meet requirements".into(),
-                    }));
+                    }))
                 }
             } else {
-                return Err(Box::new(ErrorOutput {
+                Err(Box::new(ErrorOutput {
                     error: "Validation failed! Balance does not meet requirements".into(),
-                }));
+                }))
             }
         }
         "withdraw_funds" => {
-            if let Some(RoutePayload::Balance(value)) = route.payload {
+            return if let Some(RoutePayload::Balance(value)) = route.payload {
                 if let BalanceActions::Withdraw(value) = value {
-                    return withdraw_funds(value, data);
+                    withdraw_funds(value, data)
                 } else {
-                    return Err(Box::new(ErrorOutput {
+                    Err(Box::new(ErrorOutput {
                         error: "Validation failed! Withdraw funds does not meet requirements".into(),
-                    }));
+                    }))
                 }
             } else {
-                return Err(Box::new(ErrorOutput {
+                Err(Box::new(ErrorOutput {
                     error: "Validation failed! Withdraw funds does not meet requirements".into(),
-                }));
+                }))
             }
         }
         "seed_zone" => {
-            if let Some(RoutePayload::Seed(value)) = route.payload {
+            return if let Some(RoutePayload::Seed(value)) = route.payload {
                 let SeederActions::Zone(value) = value;
-                return seed_zone(value, data);
+                seed_zone(value, data)
             } else {
-                return Err(Box::new(ErrorOutput {
+                Err(Box::new(ErrorOutput {
                     error: "Validation failed! Seed Zone does not meet requirements".into(),
-                }));
+                }))
             }
         }
         "remove_zone" => {
-            if let Some(RoutePayload::Remove(value)) = route.payload {
-                return remove_zone(value, data);
+            return if let Some(RoutePayload::Remove(value)) = route.payload {
+                remove_zone(value, data)
             } else {
-                return Err(Box::new(ErrorOutput {
+                Err(Box::new(ErrorOutput {
                     error: "Validation failed! Remove Zone does not meet requirements".into(),
-                }));
+                }))
             }
         }
         _ => todo!(),
