fedbfa0a2a7c45eb433dc2d698bcce0d7bb19ea5
dawidmazur <dawid.m@webchefs.pl>
2022-12-15 18:57:22 +0100

    landing - call to action

--- frontend/src/views/Landing/Home/HomeView.vue
+++ frontend/src/views/Landing/Home/HomeView.vue
@@ -4,10 +4,12 @@
     <Hero />
     <PrimaryFeatures />
     <SecondaryFeatures />
+    <CallToAction />
   </main>
 </template>
 
 <script setup lang="ts">
+import CallToAction from "@/components/Landing/CallToAction.vue";
 import Header from "@/components/Landing/Header.vue";
 import Hero from "@/components/Landing/Hero.vue";
 import PrimaryFeatures from "@/components/Landing/PrimaryFeatures.vue";
