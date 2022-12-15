e4c0abdbabe029898ac571faf6052dba146e3bb7
dawidmazur <dawid.m@webchefs.pl>
2022-12-15 19:08:18 +0100

    landing - faqs

--- frontend/src/views/Landing/Home/HomeView.vue
+++ frontend/src/views/Landing/Home/HomeView.vue
@@ -5,11 +5,13 @@
     <PrimaryFeatures />
     <SecondaryFeatures />
     <CallToAction />
+    <Faqs />
   </main>
 </template>
 
 <script setup lang="ts">
 import CallToAction from "@/components/Landing/CallToAction.vue";
+import Faqs from "@/components/Landing/Faqs.vue";
 import Header from "@/components/Landing/Header.vue";
 import Hero from "@/components/Landing/Hero.vue";
 import PrimaryFeatures from "@/components/Landing/PrimaryFeatures.vue";
