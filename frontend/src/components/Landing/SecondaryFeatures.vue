e55652e1b6383db072c71155d6a85e47eea28fe7
Dawid Mazur <dawid.m@webchefs.pl>
2022-12-16 18:19:05 +0100

    features icons

--- frontend/src/components/Landing/SecondaryFeatures.vue
+++ frontend/src/components/Landing/SecondaryFeatures.vue
@@ -1,6 +1,9 @@
 <template>
-  <section id="secondary-features" aria-label="Features for simplifying everyday business tasks"
-    class="pt-20 pb-14 sm:pb-20 sm:pt-32 lg:pb-32">
+  <section
+    id="secondary-features"
+    aria-label="Features for simplifying everyday business tasks"
+    class="pt-20 pb-14 sm:pb-20 sm:pt-32 lg:pb-32"
+  >
     <Container>
       <div class="mx-auto max-w-2xl md:text-center">
         <h2 class="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
@@ -16,18 +19,10 @@
         <TabList class="grid grid-cols-3 gap-x-8">
           <div v-for="(feature, index) in features" class="relative">
             <div :class="clsx('opacity-75 hover:opacity-100')">
-              <div :class="clsx(
-                'w-9 rounded-lg',
-                'bg-blue-600'
-              )">
-                <!-- <svg aria-hidden="true" className="h-9 w-9" fill="none">
-          <feature.icon />
-        </svg> -->
+              <div :class="clsx('w-9 rounded-lg', 'bg-blue-600')">
+                <component :is="feature.icon" class="text-white p-1" />
               </div>
-              <h3 :class="clsx(
-                'mt-6 text-sm font-medium',
-                'text-blue-600'
-              )">
+              <h3 :class="clsx('mt-6 text-sm font-medium', 'text-blue-600')">
                 <Tab class="[&:not(:focus-visible)]:focus:outline-none">
                   <span class="absolute inset-0" />
                   {{ feature.name }}
@@ -40,17 +35,26 @@
             </div>
           </div>
         </TabList>
-        <TabPanels class="relative mt-20 overflow-hidden rounded-4xl bg-slate-200 px-14 py-16 xl:px-16">
+        <TabPanels
+          class="relative mt-20 overflow-hidden rounded-4xl bg-slate-200 px-14 py-16 xl:px-16"
+        >
           <div class="-mx-5 flex">
-            <TabPanel v-for="(feature, index) in features" static class="px-5 transition duration-500 ease-in-out [&:not(:focus-visible)]:focus:outline-none"
-              :style="transform">
+            <TabPanel
+              v-for="(feature, index) in features"
+              static
+              class="px-5 transition duration-500 ease-in-out [&:not(:focus-visible)]:focus:outline-none"
+              :style="transform"
+            >
               <div
-                class="w-[52.75rem] overflow-hidden rounded-xl bg-white shadow-lg shadow-slate-900/5 ring-1 ring-slate-500/10">
+                class="w-[52.75rem] overflow-hidden rounded-xl bg-white shadow-lg shadow-slate-900/5 ring-1 ring-slate-500/10"
+              >
                 <img class="w-full" :src="feature.image" alt="" sizes="52.75rem" />
               </div>
             </TabPanel>
           </div>
-          <div class="pointer-events-none absolute inset-0 rounded-4xl ring-1 ring-inset ring-slate-900/10" />
+          <div
+            class="pointer-events-none absolute inset-0 rounded-4xl ring-1 ring-inset ring-slate-900/10"
+          />
         </TabPanels>
       </TabGroup>
     </Container>
@@ -61,46 +65,48 @@
 import { TabGroup, TabList, TabPanels, TabPanel, Tab } from "@headlessui/vue";
 import Container from "@/components/Landing/Container.vue";
 import clsx from "clsx";
-import { reactive, ref } from 'vue';
+import { reactive, ref } from "vue";
+import {
+  GlobeEuropeAfricaIcon,
+  CheckIcon,
+  ReceiptRefundIcon,
+} from "@heroicons/vue/24/outline";
 
-const selectedTab = ref(0)
+const selectedTab = ref(0);
 let transform = reactive({
-  transform: 'translateX(-100%)',
-})
+  transform: "translateX(-100%)",
+});
 
 function changeTab(index) {
   let shift = -100 * index;
-  transform.transform = 'translateX('+shift+'%)';
+  transform.transform = "translateX(" + shift + "%)";
   selectedTab.value = index;
 }
 
 const features = [
   {
-    name: 'GeoZones',
-    summary: 'Geojson zone upload',
+    name: "GeoZones",
+    summary: "Geojson zone upload",
     description:
-      'Upload your city zones to the system be the sole owner. Store it securely in the SQLite database.',
+      "Upload your city zones to the system be the sole owner. Store it securely in the SQLite database.",
     image: "/images/prtscn_geojson.png",
-    icon: "",
+    icon: GlobeEuropeAfricaIcon,
   },
   {
-    name: 'Validation',
-    summary:
-      'Validation',
+    name: "Validation",
+    summary: "Validation",
     description:
-      'Validate a ticket by licence plate. Give your controllers an easy tool.',
+      "Validate a ticket by licence plate. Give your controllers an easy tool.",
     image: "/images/prtscn_validator.png",
-    icon: "",
+    icon: CheckIcon,
   },
   {
-    name: 'Withdraw',
-    summary:
-      'Withdraw funds',
+    name: "Withdraw",
+    summary: "Withdraw funds",
     description:
-      'Easily withdraw funds to maintain your zone, refresh parking lines, so something good.',
+      "Easily withdraw funds to maintain your zone, refresh parking lines, so something good.",
     image: "/images/prtscn_withdraw.png",
-    icon: "",
+    icon: ReceiptRefundIcon,
   },
-]
-
+];
 </script>
